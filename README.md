094edf4322e0d00b843f55d40966e51a00aa8769
mmackovic024 <mackovic.mirko@gmail.com>
2019-08-19 15:45:38 +0200

    Auto-generated commit

--- README.md
+++
@@ -1,8 +0,0 @@
-# AngularTest
-
-SpaceXdata API integration with Angular 8
-
-- SASS styling
-- Angular routing
-- Infinite scrolling
-- Custom pipe for countdown timer
--- src/app/date-countdown.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateCountdownPipe } from './date-countdown.pipe';
-
-describe('DateCountdownPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateCountdownPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
